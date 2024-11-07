ca9b821b1f6e18dcdb1ae71fae8e31099b89aa5f
Yann Klein <yann.klein@me.com>
2024-11-07 11:55:27 +0900

    add texture to the shark

--- main.js
+++ main.js
@@ -3,54 +3,64 @@ import * as THREE from 'three';
 import { FBXLoader } from 'three/addons/loaders/FBXLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-
 // console.log(Addons);
 
-
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  10000,
+);
 
-scene.background = new THREE.Color( 0xa0a0a0 );
-
+scene.background = new THREE.Color(0x22aaff);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animate );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setAnimationLoop(animate);
+renderer.setPixelRatio(window.devicePixelRatio)
+document.body.appendChild(renderer.domElement);
 
-const controls = new OrbitControls( camera, renderer.domElement );
+const controls = new OrbitControls(camera, renderer.domElement);
 
+const light = new THREE.AmbientLight( 0xFFFFFF, 3 );
+scene.add( light );
 
+const fbxLoader = new FBXLoader();
+const texture = new THREE.TextureLoader().load( 'mechanical-shark/textures/wood_beech_chocolate_brown_v4_alt2_Diffuse.jpg' );
+texture.colorSpace = THREE.SRGBColorSpace;
 
-const fbxLoader = new FBXLoader()
 fbxLoader.load(
-    'mechanical-shark/source/woodshark.fbx',
-    (object) => {
-        object.traverse(function (child) {
-            if (child.isMesh) {
-                // child.material = material
-                if (child.material) {
-                    child.material.transparent = false
-                }
-            }
-        })
-        object.scale.set(.01, .01, .01)
-        scene.add(object)
-    },
-    (xhr) => {
-        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-    },
-    (error) => {
-        console.log(error)
-    }
-)
+  'mechanical-shark/source/woodshark.fbx',
+  (object) => {
+    object.traverse(function (child) {
+      if (child.isMesh) {
+        child.material.map = texture;
+        child.material.needsupdate = true;
+        child.material.map.minFilter = THREE.NearestFilter;
+        console.log(texture);
+        // render(); // only if there is no render loop
+        console.log(child.geometry.attributes.uv);
 
-camera.position.z = 20;
+        child.castShadow = true;
+        child.receiveShadow = true;
+      }
+    });
+    // object.scale.set(0.01, 0.01, 0.01);
+    object.rotation.x += Math.PI / 8;
+    object.rotation.y += Math.PI / 8;
+    scene.add(object);
+  },
+  (xhr) => {
+    console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+  },
+  (error) => {
+    console.log(error);
+  },
+);
 
+camera.position.z = 1500;
 
 function animate() {
-
-
-	renderer.render( scene, camera );
-
+  renderer.render(scene, camera);
 }
